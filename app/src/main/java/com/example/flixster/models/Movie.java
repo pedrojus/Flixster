1e1d5de0d3aca3df49e10ae5f22038d05ff81f75
pedrojus <p.jusino@hotmail.com>
2021-09-13 22:35:46 -0500

    Updated getBackdropPath() to return the url with original image sizing

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -36,7 +36,7 @@ public class Movie {
     }
 
     public String getBackdropPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+        return String.format("https://image.tmdb.org/t/p/original/%s", backdropPath);
     }
 
     public String getTitle() {
