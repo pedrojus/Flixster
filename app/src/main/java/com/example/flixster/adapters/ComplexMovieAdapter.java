98e097fbe0c521741f838aee0920c4c5e30a37d9
pedrojus <p.jusino@hotmail.com>
2021-09-13 22:41:27 -0500

    Incorporated heterogeneous recycler views to handle movies with higher than 5 stars

---
+++ app/src/main/java/com/example/flixster/adapters/ComplexMovieAdapter.java
@@ -0,0 +1,116 @@
+package com.example.flixster.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ComplexMovieAdapter extends RecyclerView.Adapter<ComplexMovieAdapter.ViewHolder> {
+    private final int POPULAR_MOVIE = 1;
+    Context context;
+    List<Movie> movies;
+
+    public ComplexMovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Inflates a layout from XML and returns the ViewHolder
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        ViewHolder viewHolder;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        switch (viewType) {
+            case POPULAR_MOVIE:
+                View backdropView = inflater.inflate(R.layout.item_backdrop, parent, false);
+                viewHolder = new ViewHolder(backdropView);
+                break;
+            default:
+                View defaultView = inflater.inflate(R.layout.item_movie, parent, false);
+                viewHolder = new ViewHolder(defaultView);
+                break;
+        }
+        return viewHolder;
+    }
+
+    @Override
+    // Populates data into ViewHolder
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder " + position);
+        Movie movie;
+        switch(viewHolder.getItemViewType()) {
+            case POPULAR_MOVIE:
+                ViewHolder backdropHolder = (ViewHolder) viewHolder;
+                movie = movies.get(position);
+                backdropHolder.bindBackdrop(movie);
+                break;
+            default:
+                ViewHolder defaultHolder = (ViewHolder) viewHolder;
+                movie = movies.get(position);
+                defaultHolder.bind(movie);
+                break;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (movies.get(position).getNumStars() > 5) {
+            return POPULAR_MOVIE;
+        }
+        return -1;
+    }
+
+    @Override
+    public int getItemCount() { return movies.size(); }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+
+            // if phone is in landscape, then imageUrl = backdrop image, else imageUrl = poster image
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+            } else {
+                imageUrl = movie.getPosterPath();
+            }
+
+
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.ic_launcher_foreground).into(ivPoster);
+        }
+
+        public void bindBackdrop(Movie movie) {
+            String imageUrl = movie.getBackdropPath();
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.ic_launcher_foreground).into(ivPoster);
+        }
+    }
+}
