9f65e3e3c9ccf08977b50ac640a3a0f6063e3c17
pedrojus <p.jusino@hotmail.com>
2021-09-20 22:53:43 -0500

    updated to retrieve and unwrap movie into activity

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -3,12 +3,21 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+    // movie to display
+    Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
     }
 }
