248e51965e753fdf9f8de8f622d466bff6687fd1
pedrojus <p.jusino@hotmail.com>
2021-09-21 19:57:56 -0500

    created empty activity for movie trailers

---
+++ app/src/main/java/com/example/flixster/MovieTrailerActivity.java
@@ -0,0 +1,14 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieTrailerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_trailer);
+    }
+}
