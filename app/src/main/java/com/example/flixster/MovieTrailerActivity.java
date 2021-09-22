1031f8f842240b372d7a61d5bc78e0da06456b7f
pedrojus <p.jusino@hotmail.com>
2021-09-21 20:28:14 -0500

    implemented YouTubePlayer view and play the video

--- app/src/main/java/com/example/flixster/MovieTrailerActivity.java
+++ app/src/main/java/com/example/flixster/MovieTrailerActivity.java
@@ -3,12 +3,37 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MovieTrailerActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MovieTrailerActivity extends YouTubeBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_trailer);
+
+        // temp video id -- TODO replace with movie trailer id from api
+        final String videoId = "tKodtNFpzBA";
+
+        // retrieve the player from the layout
+        YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+
+        playerView.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.cueVideo(videoId);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.e("MovieTrailerActivity", "Error initializing YouTube player");
+            }
+        });
     }
 }
